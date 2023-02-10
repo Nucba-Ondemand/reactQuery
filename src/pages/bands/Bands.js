4de3cf3d5038d7ea8739f32e0d508caab3f334a0
Rodrigo Troncoso <rodrigo-rx@hotmail.com>
2023-02-10 17:39:25 -0300

    06

--- src/pages/bands/Bands.js
+++ src/pages/bands/Bands.js
@@ -5,52 +5,29 @@ import { useQuery } from "react-query";
 import Card from "../../components/card/Card";
 import Loader from "../../components/loader/Loader";
 
-import {
-	BandsContainerStyled,
-	BandsDisabledMessageStyled,
-	BandsErrorMessageStyled,
-} from "./BandsStyles";
-import { Box, Button } from "@mui/material";
+import { BandsContainerStyled, BandsErrorMessageStyled } from "./BandsStyles";
 
 const fetchBands = () => {
 	return axios.get("http://localhost:3006/bandas");
 };
 
-//LA VERSION CON CALLBACKS Y DE ERRORS ESTÁ MAS ABAJO
-
 const Bands = () => {
-	const { isLoading, data, isError, error, isIdle, refetch } = useQuery(
-		"bands",
-		fetchBands,
-		{
-			cacheTime: 5000,
-			enabled: false,
-		}
-	);
+	const { isLoading, data, isError, error } = useQuery("bands", fetchBands, {
+		cacheTime: 4000,
+		select: (data) => {
+			const newBands = data.data.map((band) => ({ ...band, genre: "Cumbia" }));
+			return { ...data, data: newBands };
+		},
+	});
+
+	//CUSTOM HOOK
+	// const { data, isLoading, isError, error } = useBandsData();
 
 	return (
 		<>
 			{isLoading && <Loader />}
 			{isError && (
 				<BandsErrorMessageStyled>{error.message}</BandsErrorMessageStyled>
-			)}
-
-			{isIdle && (
-				<Box sx={{ width: "100%", textAlign: "center" }}>
-					<BandsDisabledMessageStyled>
-						Consulta deshablitida
-					</BandsDisabledMessageStyled>
-
-					<Button
-						sx={{
-							backgroundColor: "#4c1d95",
-						}}
-						variant="contained"
-						onClick={refetch}
-					>
-						Activar
-					</Button>
-				</Box>
 			)}
 
 			{data && (
@@ -63,57 +40,5 @@ const Bands = () => {
 		</>
 	);
 };
-
-//CON CALLBACKS
-// const Bands = () => {
-// 	const { isLoading, data, isError, error } = useQuery("bands", fetchBands, {
-// 	  onSuccess: (data) => console.log("Petición exitosa 🥳", data),
-// 	  onError: (error) => console.log("Ocurrió un error 😢", error),
-// 	  cacheTime: 4000,
-// 	});
-
-// 	return (
-// 	  <>
-// 		{isLoading && <Loader />}
-// 		{isError && (
-// 		  <BandsErrorMessageStyled>{error.message}</BandsErrorMessageStyled>
-// 		)}
-
-// 		{data && (
-// 		  <BandsContainerStyled>
-// 			{data.data.map((band) => (
-// 			  <Card key={band.id} {...band} />
-// 			))}
-// 		  </BandsContainerStyled>
-// 		)}
-// 	  </>
-// 	);
-//   };
-
-//ERRROR RETRIES
-// const Bands = () => {
-// 	const { isLoading, data, isError, error } = useQuery("bands", fetchBands, {
-// 	  retry: 2,
-// 	  retryDelay: 2000,
-// 	  cacheTime: 4000,
-// 	});
-
-// 	return (
-// 	  <>
-// 		{isLoading && <Loader />}
-// 		{isError && (
-// 		  <BandsErrorMessageStyled>{error.message}</BandsErrorMessageStyled>
-// 		)}
-
-// 		{data && (
-// 		  <BandsContainerStyled>
-// 			{data.data.map((band) => (
-// 			  <Card key={band.id} {...band} />
-// 			))}
-// 		  </BandsContainerStyled>
-// 		)}
-// 	  </>
-// 	);
-//   };
 
 export default Bands;
